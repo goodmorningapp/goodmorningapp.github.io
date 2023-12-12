97538ea676661cd9ce71a81104996fe72dd5fe77
goodmorningapp <153389196+goodmorningapp@users.noreply.github.com>
2023-12-12 20:54:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const now = new Date();
 const hours = now.getHours();
 var turn = 0;
 
-
 if (4 <= hours && hours < 12){
     good_text.textContent = 'Good Morning'
+    document.title = 'Good Morning'
 }
 else if (12 <= hours && hours < 18){
     good_text.textContent = 'Good Afternoon'
+    document.title = 'Good Afternoon'
 }
 else if (18 <= hours && hours < 22){
     good_text.textContent = 'Good Evening'
+    document.title = 'Good Evening'
 }
 else{
     good_text.textContent = 'Good Night'
+    document.title = 'Good Night'
 }
 
 var url_weather = 'https://api.openweathermap.org/data/2.5/weather?lat=47.6693&lon=9.0799&appid=51387f71b3654879e5fb657de92e89d6'
@@ -26,17 +29,20 @@ var winddirection_value = document.getElementById('winddirection_value')
 var rain_value = document.getElementById('rain_value')
 var humidity_value = document.getElementById('humidity_value')
 var rain_name = document.getElementById('rain_name')
+var arrow = document.getElementById('arrow')
 
 
 fetch(url_weather)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      /*console.log(data);*/
         temp_value.innerHTML = `${Math.floor((parseFloat(data['main']['temp']) - 273.15))}°C`
         feelslike_value.innerHTML =  `${Math.floor((parseFloat(data['main']['feels_like']) - 273.15))}°C`
         humidity_value.innerHTML =  `${data['main']['humidity']}%`
         windspeed_value.innerHTML = `${Math.floor(parseFloat(data['wind']['speed'])* 3.6)}km/h`
-        winddirection_value.innerHTML = `${data['wind']['deg']}°`
+        /*winddirection_value.innerHTML = `${Math.abs(data['wind']['deg']-180)}°`*/
+        /*console.log(Math.abs(data['wind']['deg']-180))*/
+        arrow.style.rotate = (`${Math.abs(data['wind']['deg']-180)}deg`)
         if (data['weather'][0]['main'] == 'Rain'){
             rain_name.innerHTML = 'Rain'
             rain_value.innerHTML = `${data['rain']['1h']}mm`
@@ -97,12 +103,15 @@ switch_button.addEventListener('click', () =>{
     fetch(url_weather)
         .then(response => response.json())
         .then(data => {
-          console.log(data);
+          /*console.log(data);*/
             temp_value.innerHTML = `${Math.floor((parseFloat(data['main']['temp']) - 273.15))}°C`
             feelslike_value.innerHTML =  `${Math.floor((parseFloat(data['main']['feels_like']) - 273.15))}°C`
             humidity_value.innerHTML =  `${data['main']['humidity']}%`
             windspeed_value.innerHTML = `${Math.floor(parseFloat(data['wind']['speed'])* 3.6)}km/h`
-            winddirection_value.innerHTML = `${data['wind']['deg']}°`
+            /*winddirection_value.innerHTML = `${Math.abs(data['wind']['deg']-180)}°`*/
+            arrow.style.rotate = (`${Math.abs(data['wind']['deg']-180)}deg`)
+            /*console.log(Math.abs(data['wind']['deg']-180))*/
+
             if (data['weather'][0]['main'] == 'Rain'){
                 rain_name.innerHTML = 'Rain'
                 rain_value.innerHTML = `${data['rain']['1h']}mm`
@@ -152,4 +161,11 @@ switch_button.addEventListener('click', () =>{
         
         })
    }
+})
+
+
+var train_cont = document.getElementById('train_box')
+
+train_cont.addEventListener('click', ()=>{
+    window.location.href = 'train.html'
 })
